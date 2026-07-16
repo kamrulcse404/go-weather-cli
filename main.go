6494dd6fb0f885f2b2a29ec1c212be9beb1be5e1
kamrulcse404 <kamrul.h01960@gmail.com>
2026-07-17 03:10:35 +0600

    main function refactor

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"weather/config"
 	"weather/services"
 	"weather/utils"
 )
 
 func main() {
+	if len(os.Args) < 2{
+		utils.PrintUsage()
+		return
+	}
+
+	city := strings.Join(os.Args[1:], " ")
+
 	cfg, err := config.LoadEnv()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	weather, err := services.GetWeather(cfg, "dhaka")
+	weather, err := services.GetWeather(cfg, city)
 
 	if err != nil {
 		fmt.Println("Error: ", err)
---
+++ utils/print_usage.go
@@ -0,0 +1,14 @@
+package utils
+
+import "fmt"
+
+func PrintUsage() {
+	fmt.Println()
+	fmt.Println("Weather CLI")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("  weather <city>")
+	fmt.Println()
+	fmt.Println("Example:")
+	fmt.Println("  weather Dhaka")
+}
